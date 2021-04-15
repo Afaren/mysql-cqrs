9aeb143b18702a5fa9084c811301c3851698688b
Chen Chongfa <cfachen@thoughtworks.com>
2021-04-15 22:31:30 +0800

    init

---
+++ src/main/java/com/example/cqrs/MysqlReadWriteSpearateApplication.java
@@ -0,0 +1,13 @@
+package com.example.cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlReadWriteSpearateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlReadWriteSpearateApplication.class, args);
+    }
+
+}
